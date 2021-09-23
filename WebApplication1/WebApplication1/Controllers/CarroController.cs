9f9b3f4697a8be236ec7b73d3305a2d6039324aa
yihongz <estpaulovictora@live.com>
2021-09-22 23:49:01 -0300

    Testando conexão e criando a ação Create

---
+++ WebApplication1/WebApplication1/Controllers/CarroController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CarroController : Controller
+    {
+        private readonly ISession _session;
+
+        public CarroController(ISession session)
+        {
+            _session = session;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Carro carro)
+        {
+            if(ModelState.IsValid)
+            {
+                using(ITransaction transaction = _session.BeginTransaction())
+                {
+                    await _session.SaveAsync(carro);
+                    await transaction.CommitAsync();
+                    return RedirectToAction(nameof(Create));
+                }
+            }
+
+            return View(carro);
+        }
+
+    }
+}
