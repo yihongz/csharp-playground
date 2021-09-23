fd96fb9fe5d6a73f9d5fc2470dcabbf4ae1d5227
Paulo Almeida <estpaulovictora@live.com>
2021-09-22 23:19:12 -0300

    01 - Configurações do banco de dados e Mapeando entidade carro

---
+++ WebApplication1/WebApplication1/Models/Carro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Carro
+    {
+        public virtual int CarroId { get; set; }
+        public virtual string Nome { get; set; }
+        public virtual string Placa { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/CarroMap.cs
@@ -0,0 +1,20 @@
+﻿
+using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CarroMap : ClassMap<Carro>
+    {
+        public CarroMap()
+        {
+            Id(c => c.CarroId);
+            Map(c => c.Nome);
+            Map(c => c.Placa);
+            Table("Carros");
+        }
+    }
+}
